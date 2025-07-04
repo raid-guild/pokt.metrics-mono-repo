f678d0b325d28fdeee758ed385f122cdb5a9de0f
jipstavenuiter <jipstavenuiter@gmail.com>
2025-07-04 09:58:07 -0400

    chore: frontend-scaffolding

---
+++ packages/client/src/pages/_app.tsx
@@ -0,0 +1,25 @@
+import "@/styles/globals.css";
+
+import {
+  HydrationBoundary,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import type { AppProps } from "next/app";
+import { useState } from "react";
+
+import Header from "@/components/header";
+
+export default function App({ Component, pageProps }: AppProps) {
+  const [queryClient] = useState(() => new QueryClient());
+  return (
+    <QueryClientProvider client={queryClient}>
+      <HydrationBoundary state={pageProps.dehydratedState}>
+        <Header />
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </HydrationBoundary>
+    </QueryClientProvider>
+  );
+}
