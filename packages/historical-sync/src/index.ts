8803ad851e22c0d2056f399f3375ec71eb6512db
ECWireless <elliott@coopallc.com>
2025-07-08 10:43:59 -0600

    chore: add initial historical sync script commands

---
+++ packages/historical-sync/src/index.ts
@@ -0,0 +1,9 @@
+const main = async () => {
+  // eslint-disable-next-line no-console
+  console.log('Historical sync is running...');
+
+  // eslint-disable-next-line no-console
+  console.log('Historical sync finished.');
+};
+
+main();
