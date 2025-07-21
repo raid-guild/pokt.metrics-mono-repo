a81b09c2a30a662d43e980514d1233b1d58153c8
jipstavenuiter <jipstavenuiter@gmail.com>
2025-07-21 09:18:19 -0600

    feat: update header

--- packages/client/src/components/header.tsx
+++ packages/client/src/components/header.tsx
@@ -1,9 +1,9 @@
 import { Logo } from "@/components/ui/logo";
 
 const Header = () => {
-  return <nav className="flex justify-between items-center p-4">
+  return <nav className="flex justify-between items-center p-4 bg-primary">
     <div className="flex items-center gap-2">
-      <Logo size="lg" />
+      <Logo size="lg" color="white"/>
     </div>
   </nav>;
 };
